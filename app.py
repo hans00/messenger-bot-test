11f8a6729f1eb5ffff8ce2061ce64b35353a9526
Hans <jy29825377@gmail.com>
2018-01-11 12:08:52 +0800

    log msg

--- app.py
+++ app.py
@@ -18,8 +18,6 @@ if FB_WEBHOOK_PATH != '/':
 
 @app.route(FB_WEBHOOK_PATH, methods=["GET"])
 def fb_webhook():
-	logging.info(request.args)
-	logging.info(VERIFY_TOKEN)
 	verify_token = request.args.get('hub.verify_token')
 	if verify_token == VERIFY_TOKEN:
 		return request.args.get('hub.challenge')
@@ -28,16 +26,17 @@ def fb_webhook():
 
 @app.route(FB_WEBHOOK_PATH, methods=['POST'])
 def fb_receive_message():
-    message_entries = json.loads(request.data.decode('utf8'))['entry']
-    for entry in message_entries:
-        for message in entry['messaging']:
-            if message.get('message'):
-                print("{sender[id]} says {message[text]}".format(**message))
-    return "Hi"
+	message_entries = json.loads(request.data.decode('utf8'))['entry']
+	logging.info(message_entries)
+	for entry in message_entries:
+		for message in entry['messaging']:
+			if message.get('message'):
+				logging.info("{sender[id]} says {message[text]}".format(**message))
+	return ''
 
 if __name__ == '__main__':
-    app.run(
-    	host='0.0.0.0',
-    	debug=True,
-    	port=int(os.environ.get('PORT', 33507))
-    )
+	app.run(
+		host='0.0.0.0',
+		debug=True,
+		port=int(os.environ.get('PORT', 33507))
+	)
